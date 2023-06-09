371dc052db2298c8949bc0bb22f05f6c8ca3df01
MOA -CODES <moyo.ajla@gmail.com>
2023-06-09 15:23:49 +0100

    at file 8

---
+++ 3-modules.js
@@ -0,0 +1,13 @@
+//3-modules separate files or code
+//encapsulation
+const names = require('./4-firstmodule')
+const sayHi = require('./5-secondmodule')
+const alt = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+// console.log(alt.items)
+// console.log(alt.singleperson.name)
+// // sayHi('philip')
+// // sayHi(names.harry)
+// // sayHi(names.john)
+// console.log(names)
--- 5-secondmodule.js
+++ 5-secondmodule.js
@@ -1,5 +1,5 @@
 const sayHi = (name)=>{
     console.log(`whats up ${name}`)
 }
-
+//export default
 module.exports = sayHi
