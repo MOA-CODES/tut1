0cfc9b355ddc8dd675eaf2a3ade71c611211adc1
MOA -CODES <moyo.ajla@gmail.com>
2023-06-17 15:34:59 +0100

    change  http module introduction

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const http = require('http');
 
 const server = http.createServer((req, res)  =>{
     if(req.url === '/'){
+        res.setHeader('Content-Type', 'text/html');
         res.end(`Welcome to our home page\n </p><a href="/"> go to the homepage</a> \n </p><a href="/about"> go to the about</a> `)
     }
-    if(req.url === '/about'){
-        res.end('learning against freelance')
+    else if(req.url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.end('learning against freelance\n </p><a href="/"> go to the homepage</a> \n </p><a href="/about"> go to the about</a>')
     }
-    
+    else{
     res.end(`<h1>Oops!</h1><p>We can't seem to find the page you are looking for</p><a href="/"> go to the homepage</a>`)
-    
+    }
 })
 
 server.listen(8020)
